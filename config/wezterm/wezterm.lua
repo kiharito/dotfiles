d58a12ccc0db82e5fc6785c0837b4fcd85086d8e
kiharito <kihara13611.c7d7@gmail.com>
2024-03-20 12:53:08 +0900

    add wezterm config

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,6 @@
+return {
+	font_size = 14.0,
+	window_frame = {
+		font_size = 14.0,
+	},
+}
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/config/wezterm ~/.config/wezterm
