edd8a1b8246218e04e71d37de1b7e4958f3a40d8
kiharito <kihara13611.c7d7@gmail.com>
2024-03-21 19:07:31 +0900

    add starship config

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/config/wezterm ~/.config/wezterm
 ln -s ~/dotfiles/config/mise ~/.config/mise
+ln -s ~/dotfiles/config/starship.toml ~/.config/starship.toml
