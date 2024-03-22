470d1cb46f4afab554547927e7dd31d037afcabc
kiharito <kihara13611.c7d7@gmail.com>
2024-03-22 22:04:36 +0900

    refactor wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,10 @@
-return {
-	font_size = 16.0,
-	window_frame = {
-		font_size = 16.0,
-	},
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font_size = 16.0
+config.window_frame = {
+    font_size = 16.0,
 }
+
+return config
