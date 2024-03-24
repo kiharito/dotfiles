aad384cd2dc5e9de4a321c217167735994666f87
kiharito <kihara13611.c7d7@gmail.com>
2024-03-24 22:37:39 +0900

    init nvim config

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/config/wezterm ~/.config/wezterm
 ln -s ~/dotfiles/config/mise ~/.config/mise
 ln -s ~/dotfiles/config/starship.toml ~/.config/starship.toml
+ln -s ~/dotfiles/config/nvim ~/.config/nvim
