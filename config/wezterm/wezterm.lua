74471f77be0f14dc6561b0e3af4cb0ddd421bf12
kiharito <kihara13611.c7d7@gmail.com>
2024-03-25 23:15:04 +0900

    change wezterm font to hackgen

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,5 +6,6 @@ config.font_size = 16.0
 config.window_frame = {
     font_size = 16.0,
 }
+config.font = wezterm.font("HackGen35 Console NF", {weight="Regular", stretch="Normal", style="Normal"})
 
 return config
