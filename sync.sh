57fa10b04d5db84aece3443c33a75256322c93e1
kiharito <kihara13611.c7d7@gmail.com>
2024-03-29 18:44:14 +0900

    add karabiner config

--- sync.sh
+++ sync.sh
@@ -5,6 +5,7 @@ brew bundle --no-lock --verbose --file=./Brewfile;
 
 ln -fns ~/dotfiles/.zshrc ~/.zshrc;
 ln -fns ~/dotfiles/config/starship.toml ~/.config/starship.toml;
+ln -fns ~/dotfiles/config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json;
 ln -fns ~/dotfiles/config/mise ~/.config/mise;
 ln -fns ~/dotfiles/config/nvim ~/.config/nvim;
 ln -fns ~/dotfiles/config/wezterm ~/.config/wezterm;
