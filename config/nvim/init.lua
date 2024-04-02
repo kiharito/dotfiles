8289db9e85a7fb01b8280da7548541fbdf95e2bd
kiharito <kihara13611.c7d7@gmail.com>
2024-04-02 22:34:56 +0900

    nvim options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,4 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("options")
+
 require("lazy").setup("plugins")
---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_option('clipboard', 'unnamedplus')
+vim.api.nvim_set_option_value('number', true, {})
+vim.api.nvim_set_option_value('signcolumn', 'yes:1', {})
