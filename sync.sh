45e9efb0df73d3e2b4eca823bec0f9b0d9cd4639
kiharito <kihara13611.c7d7@gmail.com>
2024-04-17 19:20:58 +0900

    add gh config.yml

--- sync.sh
+++ sync.sh
@@ -9,6 +9,7 @@ ln -fns ~/dotfiles/.gitconfig ~/.gitconfig;
 ln -fns ~/dotfiles/.ideavimrc ~/.ideavimrc;
 ln -fns ~/dotfiles/.zshrc ~/.zshrc;
 ln -fns ~/dotfiles/config/starship.toml ~/.config/starship.toml;
+ln -fns ~/dotfiles/config/gh/config.yml ~/.config/gh/config.yml;
 ln -fns ~/dotfiles/config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json;
 ln -fns ~/dotfiles/config/mise ~/.config/mise;
 ln -fns ~/dotfiles/config/nvim ~/.config/nvim;
