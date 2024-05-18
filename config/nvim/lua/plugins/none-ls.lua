25b115e3f35fdf2a1a9853609179a9b8e121d9c9
kiharito <kihara13611.c7d7@gmail.com>
2024-05-18 16:08:35 +0900

    add none-ls formatting setup

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -2,7 +2,14 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("null-ls").setup()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black
+      }
+    })
+
     vim.keymap.set("n", "<leader>fm", function()
       vim.lsp.buf.format({
         timeout_ms = 200,
