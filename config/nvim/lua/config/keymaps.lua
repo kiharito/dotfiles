b32338565817ef0e937499d627d8e87ec6d1b4cf
kiharito <kiharito@users.noreply.github.com>
2025-05-04 18:17:50 +0900

    wip

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+if vim.g.vscode then
+  local map = vim.keymap.set
+  local opts = { noremap = true, silent = true }
+
+  map("n", "<leader>e", function()
+    vim.fn.VSCodeNotify("workbench.view.explorer")
+  end, opts)
+end
