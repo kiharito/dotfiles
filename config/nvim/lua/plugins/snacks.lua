81d7972fc893f561804ca1e7de1a2f76962eea50
kiharito <kiharito@users.noreply.github.com>
2025-07-05 15:17:19 +0900

    add bufdelete

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -4,15 +4,19 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
+    bufdelete = { enabled = true },
     explorer = { enabled = true },
-    picker = { enabled = true },
     lazygit = { enabled = true },
+    picker = { enabled = true },
   },
   keys = {
     -- Top Pickers & Explorer
     { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
     { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
     { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- Bufdelete
+    { "<leader>bd", function() Snacks.bufdelete() end, desc = "Delete Buffer" },
+    { "<leader>bo", function() Snacks.bufdelete.other() end, desc = "Delete Other Buffers" },
     -- Lazygit
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
   },
