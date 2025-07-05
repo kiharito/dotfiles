7a9b77157defa89904b5a756d4cdaa83c5e9939b
kiharito <kiharito@users.noreply.github.com>
2025-07-05 14:33:12 +0900

    setup snacks.nvim

---
+++ config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    explorer = { enabled = true },
+    picker = { enabled = true },
+    lazygit = { enabled = true },
+  },
+  keys = {
+    -- Top Pickers & Explorer
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    -- Lazygit
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+  },
+}
