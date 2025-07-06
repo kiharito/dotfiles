d742875556ab9d2bd7654a927ca5cf50fb7992ea
kiharito <kiharito@users.noreply.github.com>
2025-07-06 15:27:05 +0900

    edit lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,17 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
-    theme = 'tokyonight'
+    options = {
+      theme = "tokyonight",
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
   },
 }
